61a3945dd4c99a89d3abfbf1975dbba14f362260
Nico262710rp <nraigoso36@ucatolica.edu.co>
2024-08-21 13:48:21 -0500

    Agregacion de cada clase

---
+++ untitled/src/Figuras.java
@@ -0,0 +1,19 @@
+
+
+
+
+import java.util.*;
+public class Figuras {
+    private Vector<Figura> figuras = new Vector<Figura>();
+
+    public void addFigura(Figura f) {
+        figuras.add(f);
+    }
+
+    public void dibujarFiguras() {
+        Enumeration<Figura> enumFiguras = figuras.elements();
+        while (enumFiguras.hasMoreElements()) {
+            enumFiguras.nextElement().dibujar();
+        }
+    }
+}
